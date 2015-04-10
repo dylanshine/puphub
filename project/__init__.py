0470c128c9706d612a435393b0b8e8ea9dd130a8
Dylan Shine <dylanshinenyc@gmail.com>
2015-04-09 22:22:02 -0400

    Renamed default to main, set up user auth system with bcrypt, mail and login_manager

--- project/__init__.py
+++ project/__init__.py
@@ -1,11 +1,25 @@
 from flask import Flask
+from flask.ext.bcrypt import Bcrypt
+from flask.ext.login import LoginManager
+from flask_mail import Mail
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+bcrypt = Bcrypt(app)
+mail = Mail(app)
 db = SQLAlchemy(app)
 
-from project.default.views import default_blueprint
+from project.main.views import main_blueprint
+from project.user.views import user_blueprint
 
-app.register_blueprint(default_blueprint)
+app.register_blueprint(main_blueprint)
+app.register_blueprint(user_blueprint)
+
+from project.models import User
+
+login_manager.login_view = "user.login"
+login_manager.login_message_category = "danger"
