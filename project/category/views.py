dcde32fa5af83c9479dd37529b5f16b633a13028
Dylan Shine <dylanshinenyc@gmail.com>
2015-04-11 15:49:00 -0400

    removed helper import in category/views.py

--- project/category/views.py
+++ project/category/views.py
@@ -2,7 +2,6 @@ from flask import render_template, Blueprint, url_for, \
     redirect, flash, request
 
 from project.models import Category, Webinar
-from .helpers import slugify
 
 category_blueprint = Blueprint('category', __name__,)
 
